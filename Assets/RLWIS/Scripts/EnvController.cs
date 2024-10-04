4c9ea8a873f9ed40825b390eb272783860724d20
spoonytripod <leetoday2@gmail.com>
2024-10-04 13:47:45 +0900

    feat : 이미지 수량 설정 및 그에 따른 Exception error 처리 로직 추가

--- Assets/RLWIS/Scripts/EnvController.cs
+++ Assets/RLWIS/Scripts/EnvController.cs
@@ -1,7 +1,7 @@
-using System.IO;
 using UnityEngine;
 using Unity.MLAgents;
 using OpenCVForUnity.UnityUtils;
+using System;
 
 public class EnvController : MonoBehaviour
 {
@@ -10,6 +10,7 @@ public class EnvController : MonoBehaviour
     private PoseCalculator poseCalculator;
 
     [Space(10)]
+    public int imgAmount;
     [Range(1f, 10f)] public float randPosInnerRatio;
     [Range(1f, 10f)] public float randPosOuterRatio;
     public float goalDistance;
@@ -51,7 +52,7 @@ public class EnvController : MonoBehaviour
         Matrix4x4 ARM = poseCalculator.ARM_Object;
 
         // Set Random values
-        Vector3 randPos = Random.onUnitSphere * Random.Range(goalDistance * randPosInnerRatio, goalDistance * randPosOuterRatio);
+        Vector3 randPos = UnityEngine.Random.onUnitSphere * UnityEngine.Random.Range(goalDistance * randPosInnerRatio, goalDistance * randPosOuterRatio);
         Quaternion randRot = GoalTrans.rotation; // 6DOF 랜덤 Rotation  적용하기 전까지 이대로 사용
 
         // Set position and rotation of the Goal
@@ -72,13 +73,31 @@ public class EnvController : MonoBehaviour
     {
         targetImgIndex += 1;
         SetTarget();
-        poseCalculator.DetectMarkers();
+        try
+        {
+            poseCalculator.DetectMarkers();
+        }
+        catch (NullReferenceException ex)
+        {
+            targetImgIndex = 0;
+            SetTarget();
+            poseCalculator.DetectMarkers();
+        }
     }
 
     public void PrevTarget()
     {
         targetImgIndex -= 1;
         SetTarget();
-        poseCalculator.DetectMarkers();
+        try
+        {
+            poseCalculator.DetectMarkers();
+        }
+        catch (NullReferenceException ex)
+        {
+            targetImgIndex = imgAmount - 1;
+            SetTarget();
+            poseCalculator.DetectMarkers();
+        }
     }
 }
